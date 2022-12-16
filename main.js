fea5030f8429cf0b69b398c547a8b5d7081e5d88
ArunRaj2312 <arunvijay1043534@gmail.com>
2022-12-15 19:07:38 -1200

    add file

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-
-let name=prompt("enter your name");
-let n=parseInt(prompt("enter the value"));
-for(i=1;i<=n;i++){
-    document.write(i+" ."+name +"<br>");
+function myFunc(){
+    let name=prompt("enter your name");
+    let n=parseInt(prompt("enter the value"));
+    for(i=1;i<=n;i++){
+        document.write(i+" ."+name +"<br>");
+    }
 }
+myFunc();
